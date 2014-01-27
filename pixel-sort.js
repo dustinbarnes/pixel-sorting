f678929b9d33720ccf0d3b47b5011ac07d23ab53
Dustin Barnes <dustin.barnes@gmail.com>
2014-01-27 13:30:42 -0800

    Adding the ability to chain transformations for more interesting effects

--- pixel-sort.js
+++ pixel-sort.js
@@ -5,20 +5,52 @@ var fs = require('fs');
 var png = require('pngjs').PNG;
 var _ = require('underscore');
 
-var invert = require('./lib/invert.js');
-var blackAndWhite = require('./lib/black-and-white.js');
-var pixelSortBlack = require('./lib/pixel-sort-black.js');
-var pixelSortWhite = require('./lib/pixel-sort-white.js');
-var pixelSortBrightness = require('./lib/pixel-sort-brightness.js');
+var allTransforms = {
+    invert: require('./lib/invert.js'),
+    blackAndWhite: require('./lib/black-and-white.js'),
+    pixelSortBlack: require('./lib/pixel-sort-black.js'),
+    pixelSortWhite: require('./lib/pixel-sort-white.js'),
+    pixelSortBrightness: require('./lib/pixel-sort-brightness.js')
+};
 
 commander
-	.version('0.0.1')
-	.option('-f, --file <filename>', "Path to file to sort")
-	.parse(process.argv);
+    .version('0.0.1')
+    .option('-f, --file <filename>', "Path to file to sort")
+    .option('-t, --transformation <transformation...>', 'Transformations to apply to image. To do multiple transformations, separate with commas');
+
+commander.on('--help', function() {
+    console.log("  Valid Transformations:");
+    console.log("");
+    _.each(allTransforms, function(obj, key) {
+        console.log("    " + key + ": " + obj.description);
+    })
+
+});
+
+commander.parse(process.argv);
+
+if ( !commander.file || !commander.transformation )
+{
+    commander.outputHelp();
+    process.exit(1);
+}
+
+var transforms = commander.transformation.replace(/ /g, '').split(',');
+console.dir(transforms);
 
 fs.createReadStream(commander.file)
-	.pipe(new png())
-	.on('parsed', function() {
-		invert.draw(this);
+    .pipe(new png())
+    .on('parsed', function() {
+        var image = this;
+
+        _.each(transforms, function(transform) {
+            if ( transform in allTransforms ) {
+                allTransforms[transform].draw(image);
+            } else {
+                console.warn("Transform " + transform + " is not valid");
+                process.exit(1);
+            }
+        });
+
         this.pack().pipe(fs.createWriteStream('out.png'));
-	});	
+    }); 
