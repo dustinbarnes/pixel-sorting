90ffca1c73f9a9311bb5566899651baf492eb4d0
Dustin Barnes <dustin.barnes@gmail.com>
2014-01-27 19:26:16 -0800

    some channel-specific sorters, and a horizonal/vertical-only sorter for the black mode

--- pixel-sort.js
+++ pixel-sort.js
@@ -9,18 +9,24 @@ var allTransforms = {
     invert: require('./lib/invert.js'),
     blackAndWhite: require('./lib/black-and-white.js'),
     pixelSortBlack: require('./lib/pixel-sort-black.js'),
+    pixelSortBlackHoriz: require('./lib/pixel-sort-black-horiz.js'),
+    pixelSortBlackVert: require('./lib/pixel-sort-black-vert.js'),
     pixelSortWhite: require('./lib/pixel-sort-white.js'),
     pixelSortBrightness: require('./lib/pixel-sort-brightness.js'),
+    pixelSortGreenChannel: require('./lib/pixel-sort-green-channel.js'),
+    pixelSortRedChannel: require('./lib/pixel-sort-red-channel.js'),
+    pixelSortBlueChannel: require('./lib/pixel-sort-blue-channel.js'),
     redBlueSwitch: require("./lib/red-blue-switch.js"),
     redGreenSwitch: require('./lib/red-green-switch.js'),
     blueGreenSwitch: require('./lib/blue-green-switch.js'),
-    normalizeBrightness: require('./lib/normalize-brightness.js')
+    //normalizeBrightness: require('./lib/normalize-brightness.js')
 };
 
 commander
     .version('0.0.1')
     .option('-f, --file <filename>', "Path to file to sort")
-    .option('-t, --transformation <transformation...>', 'Transformations to apply to image. To do multiple transformations, separate with commas');
+    .option('-t, --transformation <transformation...>', 'Transformations to apply to image. To do multiple transformations, separate with commas')
+    .option('--all', 'Apply all transforms to the given file')
 
 commander.on('--help', function() {
     console.log("  Valid Transformations:");
@@ -33,28 +39,39 @@ commander.on('--help', function() {
 
 commander.parse(process.argv);
 
-if ( !commander.file || !commander.transformation )
+if ( !commander.file || (!commander.all && !commander.transformation) )
 {
     commander.outputHelp();
     process.exit(1);
 }
 
-var transforms = commander.transformation.replace(/ /g, '').split(',');
-console.dir(transforms);
+if ( commander.all ) {
+    _.each(Object.keys(allTransforms), function(tf) {
+        doTransform([tf], tf + '.png');
+    });
+} else {
+    doTransform(commander.transformation.replace(/ /g, '').split(','));
+}
 
-fs.createReadStream(commander.file)
-    .pipe(new png())
-    .on('parsed', function() {
-        var image = this;
+function doTransform(transforms, outputFile)
+{
+    outputFile = outputFile || 'out.png';
 
-        _.each(transforms, function(transform) {
-            if ( transform in allTransforms ) {
-                allTransforms[transform].draw(image);
-            } else {
-                console.warn("Transform " + transform + " is not valid");
-                process.exit(1);
-            }
-        });
+    fs.createReadStream(commander.file)
+        .pipe(new png())
+        .on('parsed', function() {
+            var image = this;
 
-        this.pack().pipe(fs.createWriteStream('out.png'));
-    }); 
+            _.each(transforms, function(transform) {
+                if ( transform in allTransforms ) {
+                    allTransforms[transform].draw(image);
+                } else {
+                    console.warn("Transform " + transform + " is not valid");
+                    process.exit(1);
+                }
+            });
+
+            this.pack().pipe(fs.createWriteStream(outputFile));
+        }); 
+}
+
